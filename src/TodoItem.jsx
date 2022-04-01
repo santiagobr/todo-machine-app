76ac9fff42c55fdb1a8a7254430bd9b418cf1769
santiagobr <santiagobastidasrodriguez@gmail.com>
2022-04-01 07:39:28 -0500

    changes about structure and icons

--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -5,13 +5,13 @@ function TodoItem(props) {
   return (
     <li className="TodoItem">
       <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}>
-        √
+        ✅
       </span>
       <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>
         {props.text}
       </p>
       <span className="Icon Icon-delete">
-        X
+        ❌
       </span>
     </li>
   );
