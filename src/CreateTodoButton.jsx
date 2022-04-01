c474b747720d92e74c7fadccc50315e76610bc53
santiagobr <santiagobastidasrodriguez@gmail.com>
2022-04-01 06:37:27 -0500

    styles to CreateTodoButton component

--- src/CreateTodoButton.jsx
+++ src/CreateTodoButton.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import './CreateTodoButton.css';
 
 function CreateTodoButton(props) {
-  return <button>+</button>;
+  return <button className='CreateTodoButton'>+</button>;
 }
 
 export { CreateTodoButton };
