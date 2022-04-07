15c8d0399687982cafc39b8e2969528b7ffa967e
santiagobr <santiagobastidasrodriguez@gmail.com>
2022-04-07 05:09:10 -0500

    listening the events

--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -2,15 +2,26 @@ import React from 'react';
 import './TodoItem.css';
 
 function TodoItem(props) {
+  const onComplete = () => {
+    alert(`Ya completaste el Todo ${props.text}`);
+  };
+
+  const onDelete = () => {
+    alert(`Borraste el Todo ${props.text}`);
+  };
+
   return (
     <li className="TodoItem">
-      <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}>
+      <span
+        className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
+        onClick={onComplete}
+      >
         ✅
       </span>
       <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>
         {props.text}
       </p>
-      <span className="Icon Icon-delete">
+      <span className="Icon Icon-delete" onClick={onDelete}>
         ❌
       </span>
     </li>
